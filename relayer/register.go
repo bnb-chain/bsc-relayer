3724d8b2708afadef3c5a6fc9daefc8ad4a5c906
emailtovamos <emailtovamos@gmail.com>
2023-02-22 20:03:27 +0000

    Check being a provisional relayer as well

--- relayer/register.go
+++ relayer/register.go
@@ -5,10 +5,22 @@ import (
 )
 
 func (r *Relayer) registerRelayerHub() {
-	_, err := r.bscExecutor.AcceptBeingRelayer(r.manager)
+	isProvisionalRelayer, err := r.bscExecutor.IsProvisionalRelayer()
 	if err != nil {
 		panic(err)
 	}
+	if isProvisionalRelayer {
+		common.Logger.Info("This relayer is a provisional relayer")
+	}
+
+	acceptBeingRelayer, err := r.bscExecutor.AcceptBeingRelayer(r.manager)
+	if err != nil {
+		panic(err)
+	}
+	if acceptBeingRelayer {
+		common.Logger.Info("This relayer has accepted to be a full relayer")
+	}
+
 	isRelayer, err := r.bscExecutor.IsRelayer()
 	if err != nil {
 		panic(err)
